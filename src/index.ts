ce81d30645afe2874fd1df8ebd0f03e05ddec0d9
Andrey Vasin <vofuss@gmail.com>
2018-03-28 08:54:59 +0400

    add mnist lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { Network, TrainSet } from "./network";
+const mnist = require("mnist");
+
+const {training, test} = mnist.set(100, 10);
+
+interface MnistItem {
+	input: number[];
+	output: number[];
+}
 
 const trainSet: TrainSet = [
 	{ inputs: [1, 0], targets: [1, 0] },
@@ -14,21 +22,42 @@ const trainSet_02: TrainSet = [
 	{ inputs: [1, 1], targets: [0] }
 ];
 
-// const nn = new Network(2, 4, 2, 0.285);
-//
-// console.time("Train");
-// // testNN.train(trainSet, 10);
-// nn.train(trainSet, 25000);
-// console.timeEnd("Train");
+const MnistTrainSet: TrainSet = training.map((item: MnistItem) => {
+	return {
+		inputs: item.input,
+		targets: item.output
+	};
+});
+
+const MnistTestSet: TrainSet = test.map((item: MnistItem) => {
+	return {
+		inputs: item.input,
+		targets: item.output
+	};
+});
+
+
+
+const nn = new Network(784, 784, 10, 0.285);
+
+console.time("Train");
+nn.train(MnistTrainSet, 1000);
+console.timeEnd("Train");
 
 (async () => {
-	// await Network.serialize(nn, "test-model-02.json");
-	const nn: Network = await Network.deserialize("test-model-02.json");
+	try {
+		// await Network.serialize(nn, "mnist-model.json");
+		// const nn: Network = await Network.deserialize("mnist-model.json");
 
-	for (const item of trainSet) {
-		const result = nn.query(item.inputs);
-		console.log();
-		console.log(result);
-		// console.log(`----------- ${result.get(0, 0) >= 0.85 || result.get(0, 1) >= 0.85 ? "TRUE" : "FALSE"} ------------`);
+		for (const item of MnistTestSet) {
+			const result = nn.query(item.inputs);
+			console.log(result);
+			console.log("- - - - - - - -");
+			console.log(item.targets);
+			console.log("============================");
+			// console.log(`----------- ${result.get(0, 0) >= 0.85 || result.get(0, 1) >= 0.85 ? "TRUE" : "FALSE"} ------------`);
+		}
+	} catch (err) {
+		console.error(err);
 	}
 })();
